ade5acf5415341f53a53716d86e24f6752f53335
Harshith R <harshith.raju0710@gmail.com>
2024-10-10 15:25:11 +0530

    changed the sidemenu bar color

--- app/components/SideMenubar.js
+++ app/components/SideMenubar.js
@@ -3,59 +3,59 @@ import React from 'react'
 const SideMenubar = () => {
   return (
     <>
-        <div className="sidebar-container hidden lg:flex flex-col w-[250px] h-screen bg-zinc-300 px-4 fixed mt-[64.8px] ">
+        <div className="sidebar-container hidden lg:flex flex-col w-[250px] h-screen bg-zinc-200/50 px-4 fixed mt-[64.8px] ">
             <div className='flex justify-center mb-5 items-center cursor-pointer mt-5 px-5 py-2 bg-black/80 text-white shadow-md shadow-black/7 rounded-md'>
                 <h2>New Chat +</h2>
             </div><div className='search-history-container overflow-scroll flex flex-col justify-center items-center gap-5'>
                 <div>
                     <p>Search history goes here</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
-                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-black/40 text-white shadow-md shadow-black/7 rounded-md'>
+                <div className='flex justify-center items-center cursor-pointer w-full px-2 py-2 bg-white/95 text-black shadow-md shadow-black/7 rounded-md'>
                     <p>Previous chats</p>
                 </div>
             </div>
